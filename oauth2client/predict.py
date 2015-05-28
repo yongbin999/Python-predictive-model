6e77a1c596fa607b69f9f7188c8e83abd2acfd4a
yongbin999 <yongbinliang@umass.edu>
2015-05-28 14:33:34 -0400

    downloaded the app libaries, need to figure out how it works

---
+++ oauth2client/predict.py
@@ -0,0 +1,17 @@
+import httplib2, webapp2
+from oauth2client.appengine import AppAssertionCredentials
+from apiclient.discovery import build
+
+http = AppAssertionCredentials('https://www.googleapis.com/auth/prediction').authorize(httplib2.Http())
+service = build('prediction', 'v1.6', http=http)
+    
+class MakePrediction(webapp2.RequestHandler):
+    def get(self):
+      result = service.hostedmodels().predict(project='414649711441', hostedModelName='sample.sentiment', body={'input': {'csvInstance': ['hello']}}).execute()
+
+      self.response.headers['Content-Type'] = 'text/plain'
+      self.response.out.write('Result: ' + repr(result))
+
+app = webapp2.WSGIApplication([
+      ('/makePrediction', MakePrediction),
+], debug=True)
