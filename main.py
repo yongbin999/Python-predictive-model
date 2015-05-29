d71e73f19d292a7a4082d5499db657f999deff2f
yongbin999 <yongbinliang@umass.edu>
2015-05-28 21:40:51 -0400

    figuring out flow crdential

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python
+import webapp2
+from predict import *
+
+from apiclient.discovery import build
+from oauth2client.client import AccessTokenRefreshError
+from oauth2client.client import OAuth2WebServerFlow
+
+client_id = '243659480938-dhm2hfpti0gmvbvsntue8ipgcq9gnb1h.apps.googleusercontent.com'
+client_secret = 'wdwckrdBZTK0ypOOjm7m_7JB'
+scope = 'https://www.googleapis.com/auth/prediction'
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+	flow = OAuth2WebServerFlow(
+			client_id=client_id,
+			client_secret=client_secret,
+                        scope=scope,
+			access_type = 'offline',
+			redirect_uri=self.request.relative_url('/auth_return')
+
+	authorize_url = flow.step1_get_authorize_url()
+	self.redirect(authorize_url);
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
--- predict.py
+++ predict.py
@@ -3,12 +3,14 @@ import httplib2, webapp2
 from oauth2client.appengine import AppAssertionCredentials
 from apiclient.discovery import build
 
-http = AppAssertionCredentials('https://www.googleapis.com/auth/prediction').authorize(httplib2.Http())
+scope = 'https://www.googleapis.com/auth/prediction'
+
+http = AppAssertionCredentials(scope).authorize(httplib2.Http())
 service = build('prediction', 'v1.6', http=http)
     
 class MakePrediction(webapp2.RequestHandler):
     def get(self):
-      result = service.hostedmodels().predict(project='414649711441', hostedModelName='sample.sentiment', body={'input': {'csvInstance': ['hello']}}).execute()
+      result = service.trainedmodels().predict(project='243659480938', id='trainlang', body={'input': {'csvInstance': ['hello']}}).execute()
 
       self.response.headers['Content-Type'] = 'text/plain'
       self.response.out.write('Result: ' + repr(result))
